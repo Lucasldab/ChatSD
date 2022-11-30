b996e623e0e1f49594e75df54b5858ce2781d855
Lucas Barros <lucasldabarros@gmail.com>
2022-11-30 12:47:50 -0300

    Application upload

---
+++ src/ChatClient.java
@@ -0,0 +1,51 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ChatClient implements Runnable{
+    private static final String SERVER_ADDRESS = "192.168.0.23";
+    private ClientSocket clientSocket;
+    private Scanner scanner;
+
+    public ChatClient() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void start() throws IOException {
+        try{
+            clientSocket = new ClientSocket(new Socket(SERVER_ADDRESS, ChatServer.PORT));
+            System.out.println("Cliente conectado ao servidor em " + SERVER_ADDRESS + ":" + ChatServer.PORT);
+            new Thread(this).start();
+            messageLoop();
+        }finally {
+            clientSocket.close();
+        }
+    }
+
+    @Override
+    public void run() {
+        String msg;
+        while ((msg = clientSocket.getMessage()) != null) {
+            System.out.printf("Msg recebida do servidor: %s\n", msg);   
+        }
+    }
+
+    private void messageLoop() throws IOException{
+        String msg;
+        do {
+            System.out.print("Digite uma mensagem (ou sair para finalizar):\n");
+            msg = scanner.nextLine();
+            clientSocket.sendMsg(msg);
+        }while(!msg.equalsIgnoreCase("sair"));
+    }
+    public static void main(String[] args) {
+        try {
+            ChatClient client = new ChatClient();
+            client.start();
+        } catch (IOException e) {
+            System.out.println("Erro ao iniciar cliente: " + e.getMessage());
+        }
+
+        System.out.println("Cliente finalizado!");
+    }
+}
---
+++ src/ChatServer.java
@@ -0,0 +1,65 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class ChatServer {
+    public static int PORT = 4000;
+    private ServerSocket serverSocket;
+    private final List<ClientSocket> clients = new LinkedList<>();
+
+    public void start() throws IOException{
+        serverSocket = new ServerSocket(PORT);
+        System.out.println("Servidor iniciado na porta " + PORT);
+        clientConnectionLoop();
+    }
+
+    private void clientConnectionLoop() throws IOException{
+        while(true){
+            ClientSocket clientSocket = new ClientSocket(serverSocket.accept());
+            clients.add(clientSocket);
+            new Thread(() -> clientMessageLoop(clientSocket)).start();
+        }
+    }
+
+    private void clientMessageLoop(ClientSocket clientSocket) {
+        String msg;
+
+        try{ 
+            while ((msg = clientSocket.getMessage()) !=null) {
+                if("sair".equalsIgnoreCase(msg))
+                    return;
+                System.out.printf("Msg recebida do cliente %s: %s\n", clientSocket.getRemoteSocketAddress(), msg);
+                sendMsgToAll(clientSocket, msg);
+                //clientSocket.sendMsg(msg);
+            }
+        } finally {
+            clientSocket.close();
+        }
+    }
+
+    private void sendMsgToAll(ClientSocket sender, String msg){
+        Iterator<ClientSocket> iterator = clients.iterator();
+        while(iterator.hasNext()){
+            ClientSocket clientSocket = iterator.next();
+            if(!sender.equals(clientSocket)){
+                if(!clientSocket.sendMsg("cliente" + sender.getRemoteSocketAddress() + ":" + msg)){
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            ChatServer server = new ChatServer();
+            server.start();
+        } catch (IOException e) {
+            System.out.println("Erro ao iniciar o servidor: " + e.getMessage());
+        }
+
+        System.out.println("Servidor finalizado");
+    }
+}
